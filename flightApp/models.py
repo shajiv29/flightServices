5b074140f4d7fb291e35585f629fcea2882a5562
Md. Sazibur Rahman <36544022+shajiv29@users.noreply.github.com>
2022-01-28 14:40:21 +0600

    chage model relationships

--- flightApp/models.py
+++ flightApp/models.py
@@ -27,7 +27,7 @@ class Passenger(models.Model):
 
 class Reservation(models.Model):
     flight = models.ForeignKey(Flight, on_delete=models.CASCADE)
-    passenger = models.OneToOneField(Passenger, on_delete=models.CASCADE)
+    passenger = models.ForeignKey(Passenger, on_delete=models.CASCADE)
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
