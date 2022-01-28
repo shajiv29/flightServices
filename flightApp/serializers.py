4f70b5f325321ff6f80cd39266f61651e141acad
Md. Sazibur Rahman <36544022+shajiv29@users.noreply.github.com>
2022-01-29 01:08:17 +0600

    add validation

--- flightApp/serializers.py
+++ flightApp/serializers.py
@@ -42,3 +42,34 @@ class ReservationSerializer(serializers.ModelSerializer):
     class Meta:
         model=Reservation
         fields='__all__'
+
+
+# class ProductSerializer(serializers.ModelSerializer):
+# # validation
+#
+# name = serializers.CharField(error_messages={'blank': 'Please fillout the product name!'})
+#
+# price = serializers.FloatField(error_messages={'blank': 'Please fillout the product price!'})
+#
+# slug = serializers.SlugField(error_messages={'blank': 'Please fillout the product slug!'})
+#
+# size = serializers.CharField(error_messages={'blank': 'Please fillout the product size!'})
+#
+# description = serializers.CharField(error_messages={'blank': 'Please fillout the description!'})
+#
+# image = serializers.ImageField(required=False, error_messages={'blank': 'Please upload a photo image!'})
+#
+# # validation
+# category = serializers.StringRelatedField(read_only=False)
+# productType = serializers.StringRelatedField()
+# user = serializers.StringRelatedField()
+#
+# def validate(self, data):
+#     if "image" not in data:
+#         raise serializers.ValidationError("Please upload a photo image!")
+#     return data
+#
+# 
+# class Meta:
+#     model = Product
+#     fields = "__all__"
